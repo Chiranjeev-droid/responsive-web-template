fefa24a99e4ffbdfdae08c03edc5c754b09d8ac6
Chiranjeev Vishnoi <somu209e@gmail.com>
2021-08-26 23:50:58 +0530

    created responsive hamburger website

---
+++ js/resp.js
@@ -0,0 +1,9 @@
+burger=document.querySelector(".burger")
+navbar=document.querySelector(".navbar")
+navlist=document.querySelector(".nav-list")
+rightNav=document.querySelector(".rightNav")
+burger.addEventListener('click',()=>{
+    navlist.classList.toggle('v-class-resp')
+    rightNav.classList.toggle('v-class-resp')
+    navbar.classList.toggle('h-nav-resp')
+})
